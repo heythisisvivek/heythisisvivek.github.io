20466b21debc9e29d352d1f5b18c3885c41ce2e7
Vivek <thevivekworkplace@gmail.com>
2019-01-02 17:22:01 +0530

    Updation in ui

--- js/index.js
+++ js/index.js
@@ -1,12 +1,8 @@
-var navnav = document.querySelector(".nav-nav");
-var navmenu = document.getElementById("nav-menu");
-
-navnav.addEventListener("click",() => {
-	if(navmenu.className === "nav-menu") {
-		navmenu.className = "nav-res";
-		navnav.innerHTML = "&#10005;";
-	} else {
-		navmenu.className = "nav-menu";
-		navnav.innerHTML = "&#9776;";
-	}
-})
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
