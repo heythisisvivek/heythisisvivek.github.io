3513ca2c930883f36645f69e9b133a989ac3a3c4
Vivek <34084065+heythisisvivek@users.noreply.github.com>
2020-01-22 17:21:30 +0530

    Mazor Update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 function searchString(str) {
     window.location.href = "https://duckduckgo.com/?q=" + str;
 }
+
+// Javascript Event To Display Content On Load Finised
+onload = () => {
+    document.querySelector(".beforeloading").style.display = "none";
+    document.querySelector(".afterloading").style.display = "block";
+  }
