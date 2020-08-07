6d460cfec89347ca28e373698f2613cdc67bae05
heythisisvivek <thevivekworkplace@gmail.com>
2020-08-07 14:49:19 +0530

    Completely new design from scratch

---
+++ project/js/app.js
@@ -0,0 +1,36 @@
+let rotate = document.querySelector(".rotate");
+let sidebar = document.querySelector(".md__sidebar");
+let bar = document.querySelector(".bar");
+let iframe = document.querySelector("#iframe");
+
+function openSideBar() {
+    if(sidebar.clientWidth > 0) {
+        sidebar.style.opacity = "1";
+        sidebar.style.width = "0px";
+        bar.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>`;
+    } else {
+        sidebar.style.opacity = "1";
+        sidebar.style.width = "250px";
+        bar.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`;
+    }
+
+    console.log(bar.innerHTML);
+}
+
+function viewbtn(id) {
+    window.open(id);
+}
+
+function changeFrame(id) {
+    iframe.setAttribute("src", id);
+}
+
+rotate.addEventListener("click", () => {
+    if(iframe.getAttribute("class") == "iframe--md") {
+        iframe.setAttribute("class", "iframe--sm");
+        rotate.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg>`;
+    } else {
+        iframe.setAttribute("class", "iframe--md");
+        rotate.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-smartphone"><rect x="5" y="2" width="14" height="20" rx="2" ry="2"></rect><line x1="12" y1="18" x2="12.01" y2="18"></line></svg>`;
+    }
+})
