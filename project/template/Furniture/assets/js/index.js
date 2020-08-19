ed20f5705c421643cbc11d9e851d8512f7fe944d
heythisisvivek <thevivekworkplace@gmail.com>
2020-08-19 13:07:46 +0530

    New Project Added

---
+++ project/template/Furniture/assets/js/index.js
@@ -0,0 +1,7 @@
+setInterval(() => {
+    if(document.title == "Furniture") {
+        document.title = "Get great deal on festival";
+    } else {
+        document.title = "Furniture";
+    }
+}, 2000)
