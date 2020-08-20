5814657604d097ad008fc18ecfd7b24ec44aa79a
heythisisvivek <thevivekworkplace@gmail.com>
2020-08-20 09:03:32 +0530

    Project Modification

--- project/template/Furniture/assets/js/index.js
+++ project/template/Furniture/assets/js/index.js
@@ -5,3 +5,18 @@ setInterval(() => {
         document.title = "Furniture";
     }
 }, 2000)
+
+document.querySelector(".goToTop").addEventListener("click", () => {
+    window.scrollTo(0, 0);
+})
+
+window.addEventListener("scroll", () => {
+    let getScrollY = window.scrollY;
+    if(getScrollY >= 14) {
+        document.querySelector("nav").classList.add("scroll-nav");
+        document.querySelector(".goToTop").style.display = "block";
+    } else {
+        document.querySelector("nav").classList.remove("scroll-nav");
+        document.querySelector(".goToTop").style.display = "none";
+    }
+})
